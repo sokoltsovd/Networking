d0f439a7132841c9f8f22d3c1ae805f6106864cd
DmitrySokoltsov <86345844+DmitrySokoltsov@users.noreply.github.com>
2021-10-31 15:02:34 +0200

    Courses

---
+++ Networking/Course.swift
@@ -0,0 +1,56 @@
+//
+//  Course.swift
+//  Networking
+//
+//  Created by Dmitry Sokoltsov on 14.10.2021.
+//
+
+import UIKit
+
+//struct Course: Decodable {
+//
+//    let id: Int?
+//    let name: String?
+//    let link: String?
+//    let imageUrl: String?
+//    let numberOfLessons: Int?
+//    let numberOfTests: Int?
+//}
+
+
+//для аламофаер
+struct Course: Decodable {
+    
+    let id: Int?
+    let name: String?
+    let link: String?
+    let imageUrl: String?
+    let numberOfLessons: Int?
+    let numberOfTests: Int?
+    
+    init?(json: [String: Any]) {
+        
+        //создаем свойства и присваиваем ему значение по ключу "" и кастм до нужного типа данных
+        let id = json["id"] as? Int
+        let name = json["name"] as? String
+        let link = json["link"] as? String
+        let imageUrl = json["imageUrl"] as? String
+        let numberOfLessons = json["numberOfLessons"] as? Int
+        let numberOfTests = json["numberOfTests"] as? Int
+        
+        self.id = id
+        self.name = name
+        self.link = link
+        self.imageUrl = imageUrl
+        self.numberOfLessons = numberOfLessons
+        self.numberOfTests = numberOfTests
+    }
+    
+    static func getArray(from jsonArray: Any) -> [Course]? {
+        
+        guard let jsonArray = jsonArray as? Array<[String: Any]> else { return nil }
+        
+        return jsonArray.compactMap { Course(json: $0) }
+    }
+}
+
