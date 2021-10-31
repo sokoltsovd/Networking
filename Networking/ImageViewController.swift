6f3af5ea3602cd63b2a2f7c6b20b2f71d825fcf9
DmitrySokoltsov <86345844+DmitrySokoltsov@users.noreply.github.com>
2021-10-31 15:05:17 +0200

    Image and fetch

---
+++ Networking/ImageViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ImageViewController.swift
+//  Networking
+//
+//  Created by Dmitry Sokoltsov on 17.10.2021.
+//
+
+import UIKit
+import Alamofire
+
+
+class ImageViewController: UIViewController {
+    
+    private let url = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+    private let largeImageUrl = "https://i.imgur.com/3416rvI.jpg"
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var completedLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        completedLabel.isHidden = true
+        progressView.isHidden = true
+    }
+    
+    func fetchImage() {
+        
+        NetworkManager.downloadImage(url: url) { (image) in
+            self.imageView.image = image
+        }
+    }
+    
+    func fetchDataWithAlamofire() {
+        
+        AlamofireNetworkRequest.downloadImage(url: url) { (image) in
+            
+            self.activityIndicator.stopAnimating()
+            self.imageView.image = image
+        
+        }
+    }
+    
+    func downloadImageWithProgress() {
+        
+        AlamofireNetworkRequest.onProgress = { progress in
+            
+            self.progressView.isHidden = false
+            self.progressView.progress = Float(progress)
+            
+        }
+        
+        AlamofireNetworkRequest.completed = { completed in
+            
+            self.completedLabel.isHidden = false
+            self.completedLabel.text = completed
+        }
+        
+        AlamofireNetworkRequest.downloadImageWithProgress(url: largeImageUrl) { (image) in
+            
+            self.completedLabel.isHidden = true
+            self.progressView.isHidden = true
+            self.activityIndicator.stopAnimating()
+            self.imageView.image = image
+            
+            
+        }
+    }
+}
+
