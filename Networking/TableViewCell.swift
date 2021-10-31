e82b182882641b39a36ce49978b02c1dd44b9ee8
DmitrySokoltsov <86345844+DmitrySokoltsov@users.noreply.github.com>
2021-10-31 15:08:20 +0200

    TableViewCell

---
+++ Networking/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  Networking
+//
+//  Created by Dmitry Sokoltsov on 15.10.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var courseImage: UIImageView!
+    @IBOutlet weak var courseNameLabel: UILabel!
+    @IBOutlet weak var numberOfLessons: UILabel!
+    @IBOutlet weak var numberOfTests: UILabel!
+    
+}
