ea25f33d0ebfafd11271649b6eae5c5353d59be2
DmitrySokoltsov <86345844+DmitrySokoltsov@users.noreply.github.com>
2021-10-31 15:04:29 +0200

    create ImageProperties

---
+++ Networking/ImageProperties.swift
@@ -0,0 +1,20 @@
+//
+//  ImageProperties.swift
+//  Networking
+//
+//  Created by Dmitry Sokoltsov on 14.10.2021.
+//
+
+import UIKit
+
+struct ImageProperties {
+    
+    let key: String
+    let data: Data
+    
+    init?(withImage image: UIImage, forKey key: String) {
+        self.key = key
+        guard let data = image.pngData() else { return nil }
+        self.data = data
+    }
+}
