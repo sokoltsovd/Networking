53c14581757b905721123906002e0cf053ae1b76
DmitrySokoltsov <86345844+DmitrySokoltsov@users.noreply.github.com>
2021-10-31 15:01:49 +0200

    Add label

---
+++ Networking/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Networking
+//
+//  Created by Dmitry Sokoltsov on 15.10.2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
